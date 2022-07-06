6ee240238043479066e167fe19359911a86bc224
MostafaOwies <mostafaowies@yahoo.com>
2022-07-06 10:41:09 +0200

    connect to other device successfully

--- app/src/main/java/com/example/wifidirecttest/ItemAdapter.kt
+++ app/src/main/java/com/example/wifidirecttest/ItemAdapter.kt
@@ -1,48 +1,47 @@
 package com.example.wifidirecttest
 
 import android.net.wifi.p2p.WifiP2pDevice
-import android.net.wifi.p2p.WifiP2pDeviceList
-import android.net.wifi.p2p.WifiP2pManager
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wifidirecttest.databinding.PeersListItemBinding
 
 
-class ItemAdapter(private val items : Array<String>
+class ItemAdapter(private val items : MutableList<WifiP2pDevice>
 ): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
 
-    private var onClickListener: OnClickListener? = null
+    private lateinit var mOnClickListener: OnClickListener
+    interface OnClickListener {
+        fun onClick(position: Int)
+    }
+    fun setOnClickListener(onClickListener: OnClickListener){
+        mOnClickListener=onClickListener
+    }
 
-    class ViewHolder (binding:PeersListItemBinding):RecyclerView.ViewHolder(
-        binding.root){
-        val deviceName=binding.deviceName
+    class ViewHolder (itemView: View,listener:OnClickListener):RecyclerView.ViewHolder(itemView){
+        val deviceName:TextView=itemView.findViewById(R.id.device_name)
+        init {
+            itemView.setOnClickListener{
+                listener.onClick(adapterPosition)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-            PeersListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        )
+        val itemView=LayoutInflater.from(parent.context).inflate(R.layout.peers_list_item,parent,false)
+        return ViewHolder(itemView,mOnClickListener)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val context = holder.itemView.context
         val item = items[position]
 
-        holder.deviceName.text=item
-        if (onClickListener != null) {
-            onClickListener!!.onClick(position, item)
-        }
+        holder.deviceName.text=item.deviceName
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
-    interface OnClickListener {
-        fun onClick(position: Int, item: String)
-    }
-    fun setOnClickListener(onClickListener: OnClickListener){
-        this.onClickListener=onClickListener
-    }
-
 }
--- app/src/main/java/com/example/wifidirecttest/MainActivity.kt
+++ app/src/main/java/com/example/wifidirecttest/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.wifidirecttest
 
 import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.IntentFilter
+import android.net.wifi.WpsInfo
 import android.net.wifi.p2p.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
+import android.util.Log
+import android.widget.ListAdapter
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wifidirecttest.databinding.ActivityMainBinding
@@ -16,35 +20,45 @@ import java.net.InetAddress
 
 class MainActivity : AppCompatActivity() {
 
-    private var binding :ActivityMainBinding?=null
-    private lateinit var mWifiP2pManager :WifiP2pManager
+    private var binding: ActivityMainBinding? = null
+    private lateinit var mWifiP2pManager: WifiP2pManager
     private lateinit var channel: WifiP2pManager.Channel
     private var receiver: BroadcastReceiver? = null
     private var intentFilter = IntentFilter()
 
-    private var peers =ArrayList<WifiP2pDevice>()
-    private var deviceNameArray = arrayOf(String())
-    private lateinit var deviceArray: Array<WifiP2pDevice?>
-
-
+    private val peers = mutableListOf<WifiP2pDevice>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        initRecyclerView()
+
         initializeWifiP2P()
         searchForPeers()
-
     }
 
-    private var handler:Handler= Handler(object :Handler.Callback {
+
+    val peerListListener = WifiP2pManager.PeerListListener { peerList ->
+        val refreshedPeers = peerList.deviceList
+        if (refreshedPeers != peers) {
+            peers.clear()
+            peers.addAll(refreshedPeers)
+            initRecyclerView(peers)
+        }
+
+        if (peers.isEmpty()) {
+            Log.d(TAG, "No devices found")
+            return@PeerListListener
+        }
+    }
+
+    private var handler: Handler = Handler(object : Handler.Callback {
         override fun handleMessage(msg: Message): Boolean {
-            when(msg.what){
-                1 ->{
-                    val readBuff :ByteArray= msg.obj as ByteArray
-                    val tempMessage=String(readBuff,0,msg.arg1)
+            when (msg.what) {
+                1 -> {
+                    val readBuff: ByteArray = msg.obj as ByteArray
+                    val tempMessage = String(readBuff, 0, msg.arg1)
                     binding?.messegeTv?.text = tempMessage
                 }
             }
@@ -52,55 +66,48 @@ class MainActivity : AppCompatActivity() {
         }
     })
 
-    private fun initRecyclerView(){
-        binding?.devicesRv?.layoutManager=LinearLayoutManager(this)
-        val adapter=ItemAdapter(deviceNameArray)
-        binding?.devicesRv?.adapter=adapter
-      /*  adapter.setOnClickListener(object :ItemAdapter.OnClickListener{
+
+    private fun initRecyclerView(peersList: MutableList<WifiP2pDevice>) {
+        binding?.devicesRv?.layoutManager = LinearLayoutManager(this)
+        val adapter = ItemAdapter(peersList)
+        binding?.devicesRv?.adapter = adapter
+
+
+        adapter.setOnClickListener(object : ItemAdapter.OnClickListener {
             @SuppressLint("MissingPermission")
-            override fun onClick(position: Int, item: String) {
-                var device :WifiP2pDevice=deviceArray[position]!!
-                val config = WifiP2pConfig()
-                config.deviceAddress = device.deviceAddress
-                channel?.also { channel ->
-                    mWifiP2pManager?.connect(channel, config, object : WifiP2pManager.ActionListener {
-
-                        override fun onSuccess() {
-                            Toast.makeText(applicationContext,"Connected to${device.deviceName}",Toast.LENGTH_SHORT ).show()
-                        }
-
-                        override fun onFailure(reason: Int) {
-                            Toast.makeText(applicationContext,"not Connected ",Toast.LENGTH_SHORT ).show()
-                        }
-                    }
-                    )}
-            }
-
-        })*/
-    }
-
-    var peerListListener : WifiP2pManager.PeerListListener= object : WifiP2pManager.PeerListListener{
-        override fun onPeersAvailable(peerList: WifiP2pDeviceList) {
-            if (!peerList.deviceList.equals(peers)){
-                peers.clear()
-                peers.addAll(peerList.deviceList)
-                deviceNameArray= arrayOf(peerList.deviceList.size.toString())
-                deviceArray= arrayOfNulls(peerList.deviceList.size)
-                var index = 0
-                peerList.deviceList.forEach{
-                    deviceNameArray[index]=it.deviceName
-                    deviceArray[index]=it
-                    index++
+            override fun onClick(position: Int) {
+                Toast.makeText(this@MainActivity,"Device Clicked",Toast.LENGTH_SHORT).show()
+                val device = peers[0]
+                val config = WifiP2pConfig().apply {
+                    deviceAddress = device.deviceAddress
+                    wps.setup = WpsInfo.PBC
                 }
+
+                mWifiP2pManager.connect(channel, config, object : WifiP2pManager.ActionListener {
+                    override fun onSuccess() {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Connected to${device.deviceName}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    override fun onFailure(reason: Int) {
+                        Toast.makeText(this@MainActivity, "not Connected ", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                })
             }
-        }
+        })
     }
 
-    private fun initializeWifiP2P(){
-        mWifiP2pManager= getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
+
+
+    private fun initializeWifiP2P() {
+        mWifiP2pManager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
         channel = mWifiP2pManager.initialize(this, mainLooper, null)
         channel.also { channel ->
-            receiver = WiFiDirectBroadcastReceiver(mWifiP2pManager!!, channel, this)
+            receiver = WiFiDirectBroadcastReceiver(mWifiP2pManager, channel, this)
         }
 
         intentFilter = IntentFilter().apply {
@@ -112,10 +119,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     @SuppressLint("MissingPermission")
-    private fun searchForPeers(){
+    private fun searchForPeers() {
         binding?.searchBtn?.setOnClickListener {
             mWifiP2pManager.discoverPeers(channel, object : WifiP2pManager.ActionListener {
                 override fun onSuccess() {
+                    Toast.makeText(applicationContext, "found", Toast.LENGTH_SHORT).show()
                 }
 
                 override fun onFailure(reasonCode: Int) {
